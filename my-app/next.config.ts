3193a8e91e5078d1f058b08e2190ac4fdd9a3834
karansai12 <karansaimd2003@gmail.com>
2026-08-30 15:17:23 +0530

    prisma update

--- my-app/next.config.ts
+++ my-app/next.config.ts
@@ -1,7 +1,11 @@
 import type { NextConfig } from "next";
+import path from "path";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  serverExternalPackages: ["@prisma/client", "pg", "@prisma/adapter-pg"],
+  turbopack: {
+    root: path.join(__dirname),
+  },
 };
 
 export default nextConfig;
